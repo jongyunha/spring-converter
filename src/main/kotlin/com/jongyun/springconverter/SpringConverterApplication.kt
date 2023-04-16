c8090d6217976ef5eaebed08c7b58a58bbed841a
neimen-95 <neimen.95@kakaostyle.com>
2023-04-16 16:27:44 +0900

    setup

---
+++ src/main/kotlin/com/jongyun/springconverter/SpringConverterApplication.kt
@@ -0,0 +1,11 @@
+package com.jongyun.springconverter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringConverterApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringConverterApplication>(*args)
+}
